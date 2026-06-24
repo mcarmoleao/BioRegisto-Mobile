0e237bfdd18e489b45f8d1f21d746104a397ecc5
Maria do Carmo <maria.leao@ipvc.pt>
2026-06-24 11:44:32 +0100

    será que é desta?

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,5 @@
 import { Redirect } from "expo-router";
-import { useAuth } from "../lib/auth"; 
 
 export default function Index() {
-  const { session, loading } = useAuth();
-
-  if (loading) return null; // ou um spinner
-
-  if (!session) {
-    return <Redirect href="/(auth)/login" />;
-  }
-  return <Redirect href="/(tabs)/feed" />;
+  return <Redirect href="/(auth)/login" />;
 }
